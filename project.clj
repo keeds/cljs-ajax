98a1b1eb008bd2ecadf58c6aad78d0d0d3862eab
Andrew Keedle <andrew.keedle@metail.co.uk>
2019-06-24 13:58:07 +0100

    deployment details

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cljs-ajax "0.8.0"
-  :min-lein-version "2.5.2" ;;; lower can't run tests in cljc
+(defproject keeds/cljs-ajax "0.8.1"
+  :min-lein-version "2.5.2"                                 ;;; lower can't run tests in cljc
   :description "A simple Ajax library for ClojureScript"
   :url "https://github.com/JulianBirch/cljs-ajax"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cheshire "5.7.1"]
                  [com.cognitect/transit-clj "0.8.309"]
                  [com.cognitect/transit-cljs "0.8.256"]
@@ -17,11 +17,11 @@
             [lein-cljsbuild "1.1.3"]
             [lein-ancient "0.6.10"]]
   :hooks [leiningen.cljsbuild]
-  :global-vars { *warn-on-reflection* true }
+  :global-vars {*warn-on-reflection* true}
   :clean-targets ^{:protect false} ["target" "target-int" "target-test"]
   :profiles
   {:dev {:source-paths ["dev", "browser-test"]
-         :test-paths ["test"]
+         :test-paths   ["test"]
          :dependencies [[clj-time "0.14.0"]
                         [compojure "1.6.0"]
                         [fogus/ring-edn "0.3.0"]
@@ -35,28 +35,33 @@
   :cljsbuild
   {:builds
    {:dev  {:source-paths ["src"]
-           :compiler {:output-to "target/main.js"
-                      :output-dir "target"
-                      ;;; :source-map "target/main.js.map"
-                      :optimizations :whitespace
-                      :pretty-print true}}
+           :compiler     {:output-to     "target/main.js"
+                          :output-dir    "target"
+                          ;;; :source-map "target/main.js.map"
+                          :optimizations :whitespace
+                          :pretty-print  true}}
     :test {:source-paths ["src" "test"]
            :incremental? true
-           :compiler {:output-to "target-test/unit-test.js"
-                      :output-dir "target-test"
-                      ;;; :source-map "target-test/unit-test.js.map"
-                      :main ajax.test.runner
-                      :optimizations :whitespace
-                      :pretty-print true
-                      :process-shim false}}
-    :int {:source-paths ["src" "browser-test"]
-          :incremental? true
-          :compiler {:output-to "target-int/integration.js"
-                     :output-dir "target-int"
-                     ;;; :source-map "target-int/integration.js.map"
-                     :warnings {:single-segment-namespace false}
-                     :optimizations :whitespace
-                     :pretty-print true}}}}
+           :compiler     {:output-to     "target-test/unit-test.js"
+                          :output-dir    "target-test"
+                          ;;; :source-map "target-test/unit-test.js.map"
+                          :main          ajax.test.runner
+                          :optimizations :whitespace
+                          :pretty-print  true
+                          :process-shim  false}}
+    :int  {:source-paths ["src" "browser-test"]
+           :incremental? true
+           :compiler     {:output-to     "target-int/integration.js"
+                          :output-dir    "target-int"
+                          ;;; :source-map "target-int/integration.js.map"
+                          :warnings      {:single-segment-namespace false}
+                          :optimizations :whitespace
+                          :pretty-print  true}}}}
   :aliases {"clj-test"  ["test"]
             "cljs-test" ["doo" "phantom" "test" "once"]
-            "run-tests" ["do" "clean," "clj-test," "cljs-test"]})
+            "run-tests" ["do" "clean," "clj-test," "cljs-test"]}
+
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
+
+  )
